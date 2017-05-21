2eb8ba054eb1cdae034195d5c5de1919adc9fd97
ZooRtOq <mikenamsky@gmail.com>
2017-05-21 20:22:34 +0400

    Рефакторинг

--- app/src/main/java/com/jobfile/pc_n/mycolorbatton/MainActivity.java
+++ app/src/main/java/com/jobfile/pc_n/mycolorbatton/MainActivity.java
@@ -1,10 +1,9 @@
 package com.jobfile.pc_n.mycolorbatton;
 
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void str(View v) {
-        Button str = (Button) findViewById(R.id.button);
-        str.setBackgroundResource(R.color.colorAccent);
+        v.setBackgroundResource(R.color.colorAccent);
     }
 }
