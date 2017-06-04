77a14234b93df4c5710b92bfe002fb622494df35
Ilya Eremin <partissanei@gmail.com>
2017-06-04 21:08:06 +0300

    распарсил описание погоды

--- app/src/main/java/com/jobfile/pc_n/mycolorbatton/WeatherActivity.java
+++ app/src/main/java/com/jobfile/pc_n/mycolorbatton/WeatherActivity.java
@@ -4,21 +4,34 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    public static final String RETRIEVE_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=Moscow,Ru&APPID=92c07848422f27c0f91cabdae2f1db2f";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        new DownloadFilesTask().execute("http://api.openweathermap.org/data/2.5/weather?q=Moscow,Ru&APPID=92c07848422f27c0f91cabdae2f1db2f");
+        new DownloadFilesTask().execute(RETRIEVE_WEATHER_URL);
     }
 
     private void showResult(String weather) {
         TextView uiText = (TextView) findViewById(R.id.weather_data);
-        uiText.setText(weather);
+        try {
+            JSONObject weatherJson = new JSONObject(weather);
+            String mainWeather = weatherJson.getJSONArray("weather").getJSONObject(0).getString("main");
+            uiText.setText(mainWeather);
+        } catch (JSONException e) {
+            Toast.makeText(this, "Ошибка при парсе json", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     private class DownloadFilesTask extends AsyncTask<String, Void, String> {
